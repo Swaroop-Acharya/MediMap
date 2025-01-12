5c8331f70f2c7b5289e4692f7ca4d342c9d47056
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2025-01-12 17:34:13 +0530

    Added: Integration testing using test containers and REST-assured for POST method

--- product-service/src/test/java/com/medimap/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/medimap/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package com.medimap.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer= new MongoDBContainer("mongo:latest");
+
+	@LocalServerPort
+	private Integer port;
+
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI="http://localhost";
+		RestAssured.port=port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody= """
+				{
+				    "id": "67836eb0c7734e74041a1a57",
+				    "name": "Paracetamol",
+				    "description": "Fever tablet",
+				    "price": 100
+				}
+		""";
+		RestAssured.given().contentType("application/json")
+				.body(requestBody).when().post("/api/product/add")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name",Matchers.equalTo("Paracetamol"))
+				.body("description",Matchers.equalTo("Fever tablet"))
+				.body("price",Matchers.equalTo(100));
+
 	}
 
 }
