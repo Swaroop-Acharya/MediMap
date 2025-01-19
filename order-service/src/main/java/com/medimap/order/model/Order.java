3f2ed318ebbfc566e7e40b897cffffde07be1b03
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2025-01-19 21:17:19 +0530

    Fix: Ignore MySQL socket and stage other files

--- order-service/src/main/java/com/medimap/order/model/Order.java
+++ order-service/src/main/java/com/medimap/order/model/Order.java
@@ -1,4 +1,16 @@
 package com.medimap.order.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "t_orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Order {
+    
 }
