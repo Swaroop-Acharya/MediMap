ee0d603b1f0bb363c6cf0b5eae7e4ca8de45c25f
Swaroop Acharya <91172033+Swaroop-Acharya@users.noreply.github.com>
2025-01-31 15:10:52 +0530

    fixed: inventory_seq not found error

--- inventory-service/src/main/java/com/medimap/inventory/model/Inventory.java
+++ inventory-service/src/main/java/com/medimap/inventory/model/Inventory.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class Inventory {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String skuCode;
