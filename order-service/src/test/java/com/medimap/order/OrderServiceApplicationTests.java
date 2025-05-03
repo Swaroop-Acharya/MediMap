46024d99c50fdd04b68dc70fd86d125ac75cb503
swaroopcodes <swaroopacharya935@gmail.com>
2025-05-03 22:40:08 +0530

    Added: Else conditon for handling false condition

--- order-service/src/test/java/com/medimap/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/medimap/order/OrderServiceApplicationTests.java
@@ -1,71 +1,82 @@
 package com.medimap.order;
 
-import com.medimap.order.stubs.InventoryClientStub;
+
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.MySQLContainer;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.testcontainers.containers.MySQLContainer;
+
+import com.medimap.order.stubs.InventoryClientStub;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)  // Automatically set up WireMock on a random port
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-    // Declare MySQLContainer with @ServiceConnection
-    @ServiceConnection
-    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.3.0");
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
 
-    @LocalServerPort
-    private Integer port;
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 
-    // Spring Boot will manage the lifecycle of the container
-    static {
-        mySQLContainer.start(); // This will be managed by Spring Boot, no need for manual start in static block
-    }
+	static {
+		mySQLContainer.start();
+	}
 
-    @BeforeEach
-    void setup() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = port;
-    }
-
-    @Test
-    void shouldCreateOrder() {
-        // Request body for creating an order
-        String requestBody = """
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
                 {
-                    "orderNumber":"abs1232",
-                    "skuCode":"Derma Facewash",
-                    "quantity":12,
-                    "price":123
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
                 }
                 """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
-        // Set up a stub for the inventory service with WireMock
-        InventoryClientStub.stubInventoryCall("Derma Facewash", 12);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
 
-        // Make the actual API request using RestAssured
-        String responseBodyString = RestAssured.given()
-                .contentType("application/json")
-                .body(requestBody)
-                .when()
-                .post("api/order")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .extract()
-                .body()
-                .asString();
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
 
-        // Validate the response body
-        assertThat(responseBodyString, Matchers.is("Order placed successfully!"));
-    }
+	@Test
+	void shouldFailOrderWhenProductIsNotInStock() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1000
+                }
+                """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1000);
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(500);
+	}
 }
